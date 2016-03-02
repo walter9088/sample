e1ecf2b83b59b98cb9cc21c6bfbc3e91c0f26a9c
walter9088 <515771829@qq.com>
2016-03-03 03:49:03 +0800

    sb

---
+++ src/main/java/com/firefly/SampleApplication.java
@@ -0,0 +1,17 @@
+package com.firefly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 类SampleApplication.java的实现描述：TODO 类实现描述
+ * 
+ * @author walter 2016年3月3日 上午3:45:31
+ */
+@SpringBootApplication
+public class SampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplication.class, args);
+    }
+}
